ddb8c0b68d73adbf08f11edec51758a670942feb
Vlad Paulechka <vlad9.ru@gmail.com>
2023-02-16 15:26:03 +0300

    add new column use_bot_id

--- schemas/usersSchemas.py
+++ schemas/usersSchemas.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 class UserSchema(BaseModel):
     user_id: int = Field(ge=1)
     chat_id: int
+    use_bot_id: int
     block: bool = Field(default=False)
     ban: bool = Field(default=False)
 
