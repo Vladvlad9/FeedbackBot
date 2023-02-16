26565ff43542a464e5215233a8a6c1c006d6c407
Vlad Paulechka <vlad9.ru@gmail.com>
2023-02-16 15:27:15 +0300

    add new column use_bot_id in user
    add new column date_created and welcome_text in BotTG

--- schemas/botTGSchemas.py
+++ schemas/botTGSchemas.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 
 class BotTGSchema(BaseModel):
     user_id: int = Field(ge=1)
     bot_id: int = Field(ge=1)
     bot_token: str
+    date_created: datetime = Field(default=datetime.now())
+    welcome_text: str = Field(default=None)
 
 
 class BotTGInDBSchema(BotTGSchema):
