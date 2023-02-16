e46de349430075c7115178f9d03c54d699437d0a
Vlad Paulechka <vlad9.ru@gmail.com>
2023-02-16 15:28:08 +0300

    add keyboard approve_ikb

--- keyboards/inline/users/formMain.py
+++ keyboards/inline/users/formMain.py
@@ -48,6 +48,25 @@ class MyCallback(CallbackData, prefix="main"):
         )
 
     @staticmethod
+    async def approve_ikb(user_id: int, bot_id: int) -> InlineKeyboardMarkup:
+        data_main_menu = {
+            "Да": {"main": "AddWelcomeTxt", "id": user_id, "editId": bot_id},
+            "Нет": {"main": "MyBots", "id": user_id, "editId": bot_id},
+        }
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text=str(name_menu),
+                                         callback_data=MyCallback(main=target_menu['main'],
+                                                                  id=target_menu['id'],
+                                                                  editId=""
+                                                                  ).pack()
+                                         )for name_menu, target_menu in data_main_menu.items()
+                ]
+            ]
+        )
+
+    @staticmethod
     async def my_bots_ikb(user_id: int) -> InlineKeyboardMarkup:
         my_bots = await CRUDBots.get_all(user_id=user_id)
         get_bot = []
@@ -106,7 +125,11 @@ class MyCallback(CallbackData, prefix="main"):
                                                                   editId="").pack())
                 ],
                 [
-                    InlineKeyboardButton(text="💬 Добавить Чат",
+                    InlineKeyboardButton(text="📝 Текст приветствия",
+                                         callback_data=MyCallback(main="WelcomeText",
+                                                                  id=bot_id,
+                                                                  editId="").pack()),
+                    InlineKeyboardButton(text="💬 Чаты",
                                          callback_data=MyCallback(main="AddChat",
                                                                   id=bot_id,
                                                                   editId="").pack())
